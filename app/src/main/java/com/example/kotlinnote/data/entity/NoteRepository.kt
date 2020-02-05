14f678d0adfb2814f1d6c96823c8f2e27d455cb3
niww <niww@ya.ru>
2020-02-05 23:34:35 +0300

    test design

--- app/src/main/java/com/example/kotlinnote/data/entity/NoteRepository.kt
+++ app/src/main/java/com/example/kotlinnote/data/entity/NoteRepository.kt
@@ -8,27 +8,32 @@ object NoteRepository {
         notes = listOf(
             Note("Заголово 1",
                 "Текст заметки 1",
-                0x6002ee
-                ),
+                0xfff6002ee.toInt()
+
+            ),
             Note("Заголово 2",
                 "Текст заметки 2",
-                0x0000ba
+                0xfff6002ee.toInt()
+
             ),
             Note("Заголово 3",
                 "Текст заметки 3",
-                0x6002ee
+                0xfff6002ee.toInt()
+
             ),
             Note("Заголово 4",
                 "Текст заметки 4",
-                0x6002ee
+                0xfff6002ee.toInt()
+
             ),
             Note("Заголово 5",
                 "Текст заметки 5",
-                0x6002ee
+                0xfff6002ee.toInt()
+
             ),
             Note("Заголово 6",
                 "Текст заметки 6",
-                0x6002ee
+                0xfff6002ee.toInt()
             )
         )
     }
